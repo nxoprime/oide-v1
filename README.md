e0ef8f7fbbf7ca2a622a64ba4100ebe475501e61
nxoprime <nxo.prime@gmail.com>
2024-10-08 14:53:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## create-svelte
+
+```
+npm create svelte@latest
+npx svelte-add@latest tailwindcss
+npx shadcn-svelte@latest init
+npm install && npm run dev
+```
+
+#### SHADCN Components
+
+```
+npx shadcn-svelte@latest add button
+```
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+import "../app.css";
+</script>
+<div class="flex-1">
+<div class="container relative pb-10 pt-10">
+<div class="bg-background overflow-hidden rounded-[0.5rem] border shadow-xl">
+<slot></slot>
+</div>
+</div>
+</div>
+
